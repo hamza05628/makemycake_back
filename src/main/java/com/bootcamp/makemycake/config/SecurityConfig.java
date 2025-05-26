3d508c12cf7d244d4afb13c885ccc9122db10967
hamza05628 <hamidi.hamza@ofppt-edu.ma>
2025-05-26 23:16:49 +0100

    Complete version: includes all implemented options/features

--- src/main/java/com/bootcamp/makemycake/config/SecurityConfig.java
+++ src/main/java/com/bootcamp/makemycake/config/SecurityConfig.java
@@ -111,6 +111,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("https://bakethatcakes-ten.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization")); // Important pour JWT
